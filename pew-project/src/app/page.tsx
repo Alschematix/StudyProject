ddbe0d21aeb84c80691743544ea880636f663576
Alschematix <Alschematix@tutanota.com>
2023-07-21 17:28:18 -0400

    Init

---
+++ pew-project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-primary">Test</h1>
+     
+    </main>
+  )
+}
