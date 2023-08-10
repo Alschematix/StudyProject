9beb36ecf4eab9d83109cf7621aa0be9bfc5a59e
zach <102389340+zw0ng@users.noreply.github.com>
2023-08-09 18:24:53 -0700

    feat: added to math element 1 - made the card collapsible - added comments

--- ewp/src/components/CollapseEx.tsx
+++ ewp/src/components/CollapseEx.tsx
@@ -5,12 +5,13 @@ export default function CollapseEx() {
   
     return (
       <>
-        <Button onClick={onToggle} bg={"green.400"} color="white">Prerequisites</Button>
+        <Button onClick={onToggle} bg={"green.400"} color="white" w="100%" mb={5}>Prerequisites</Button>
         <Collapse in={isOpen} animateOpacity>
           <Box
             p='40px'
             color='green.500'
-            mt='4'
+            mt='0'
+            mb='4'
             bg='lightgrey'
             rounded='md'
             shadow='md'
