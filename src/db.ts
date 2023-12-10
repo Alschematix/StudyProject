467c76972c48899f934a7f8324b907a6bc867cdc
Jonathan Biemond <jbiemond@my.bcit.ca>
2023-12-10 07:37:03 -0800

    feat(db): add fetchContent and fetchPageData functions

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,28 @@
 // database connection variables
-import { drizzle } from 'drizzle-orm/postgres-js'
+import {drizzle} from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as dotenv from 'dotenv';
+import * as contentSchema from './db/schema/content';
+import * as pageSchema from './db/schema/page';
+import * as textSchema from './db/schema/text';
+import * as topicSchema from './db/schema/topic';
+import * as userSchema from './db/schema/user';
+
 
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL
 const client = postgres(connectionString || '', {max: 1})
-const db = drizzle(client);
+const db = drizzle(
+    client,
+    {
+        schema: {
+            ...contentSchema,
+            ...pageSchema,
+            ...textSchema,
+            ...topicSchema,
+            ...userSchema,
+        }
+    });
 
-export { db };
+export {db};
--- src/db/operations.ts
+++ src/db/operations.ts
@@ -1,9 +1,16 @@
 // Database CRUD operations
 import {textContent} from './schema/text';
 import {content} from './schema/content';
-import {drizzle} from "drizzle-orm/postgres-js";
+import {PostgresJsDatabase} from "drizzle-orm/postgres-js";
+import {page, slot} from "./schema/page";
+import {eq, inArray} from "drizzle-orm";
+import {db} from '../db';
 
-async function insertTextContent(db: ReturnType<typeof drizzle>, data: (typeof textContent.$inferInsert)[]) {
+
+/*
+ * Insert text content into the database.
+ */
+async function insertTextContent(db: PostgresJsDatabase, data: (typeof textContent.$inferInsert)[]) {
 
     await db.transaction(async (db) => {
 
@@ -16,12 +23,71 @@ async function insertTextContent(db: ReturnType<typeof drizzle>, data: (typeof t
             result.map(({textContentId}) => ({
                     type: 'text',
                     address: textContentId.toString(),
-                }
-            ))
+                })
+            )
         )
 
     });
 
+}
+
+interface Content {
+    address: string;
+    type: string;
+    value: string;
+}
+
+/*
+ * Fetch content from the database.
+ */
+export const fetchContent = async (contentIds: number[]) => {
+    const contentMap = new Map<number, Content | undefined>();
+    const contents = await db.query.content.findMany({
+        where: inArray(content.id, contentIds),
+    })
+    for (const {address, id, type} of contents) {
+        if (type === 'text') {
+            const result = await db.select({
+                value: textContent.body,
+            }).from(textContent).where(eq(textContent.id, parseInt(address)));
+            if (result.length !== 1) {
+                contentMap.set(id, undefined);
+            }
+            const {value} = result[0];
+            contentMap.set(id, {address, type: type, value});
+        } else {
+            throw new Error(`Unknown content type: ${type}`);
+        }
+    }
+    return contentMap;
+}
+
+/*
+ * Fetch page data and content from the database.
+ */
+export const fetchPageData = async (pageId: number) => {
+    // Query the page
+    // This should be able to be done in one query.
+    const resultPage = await db.query.page.findFirst({
+        where: eq(page.id, pageId),
+    });
+    if (!resultPage) {
+        throw new Error(`Page ${pageId} not found`);
+    }
+
+    // Query the slots
+    const slots = await db.query.slot.findMany({
+        where: eq(slot.pageId, pageId)
+    });
+    if (slots.length === 0) {
+        return {resultPage, content: []};
+    }
+
+    // Fetch the content
+    const contentIds = slots.map((slot: { contentId: number }) => slot.contentId);
+    const contentMap = await fetchContent(contentIds);
+
+    return {resultPage, content: Array.from(contentMap.values())};
 }
 
 export {insertTextContent};
